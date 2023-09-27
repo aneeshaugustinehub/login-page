fbfea71b82ee9a764bcf3c2becca9521720a4884
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2023-09-27 16:52:11 +0530

    routes added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import  express from "express";
-const port =3000;
+import dotenv from "dotenv";
+import userRoutes from './routes/userRoutes.js'
+
+dotenv.config();
+import { notFound,errorHandler } from "./middleware/errorMiddileware.js";
+const port = process.env.PORT || 5000;
+
 const app = express();
+
+app.use('/api/users', userRoutes)
+
 app.get('/',(req,res) => res.send('server is ready'));
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(port,()=> console.log(`server started on port ${port}`));
